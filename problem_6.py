41a30ed2912978fb0a0e20f617d8caedd0401bd5
apu <40669057+xrayapu@users.noreply.github.com>
2024-05-04 14:01:27 +0600

    Create problem_6.py

---
+++ problem_6.py
@@ -0,0 +1,49 @@
+# problem - 6: maximum area on histogram. we use, nsr and nsl to find the width and then use area formula 
+
+from collections import deque
+def nsr(arr):
+    temp=[len(arr)]*len(arr) # value in right side on the array
+    s=deque()
+    for i in reversed(range(len(arr))):
+        while s:
+            if s[-1][0] < arr[i]:
+                temp[i]= s[-1][1]
+                break
+            else: 
+                s.pop()
+
+        s.append((arr[i],i))
+
+    return temp
+def nsl(arr):
+    temp=[-1]*len(arr)
+    s=deque()
+    for i in range(len(arr)):
+        while s:
+            if s[-1][0] < arr[i]:
+                temp[i]= s[-1][1]
+                break
+            else:
+                s.pop()
+
+        s.append((arr[i],i))
+
+    return temp
+def sol(arr):
+    width, ans=[],[]
+    right= nsr(arr)
+    left= nsl(arr)
+    #print(right,left)
+
+    for i in range(len(arr)):
+        width.append( (right[i] - left[i] -1))
+
+    
+    for i in range(len(arr)):
+        ans.append(( arr[i] * width[i]))
+
+    #print(ans)
+
+    return max(ans)
+
+print(sol([6, 2, 5, 4, 5, 1, 6]))
